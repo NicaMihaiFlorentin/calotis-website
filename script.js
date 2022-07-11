b982f937d419ba4182790f4f271426d4ce8e798a
Mihai <83167717+NicaMihaiFlorentin@users.noreply.github.com>
2022-07-11 12:47:59 +0300

    Inital Commit

---
+++ script.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    $('head').append('<link href="https://u.nya.is/mzrdtf.png" rel="shortcut icon" type="image/x-icon" />');
+    
+    $.getJSON("https://ptb.discordapp.com/api/guilds/264370940706553857/widget.json", function(result) {
+
+        for(i=0; i < result.channels.length; i++){
+            if(result.channels[i].position === 0){
+                var serverNum = result.channels[i].name.replace(":", "");
+            } else if (result.channels[i].position === 1){
+                var usersNum = result.channels[i].name.replace(":", "");
+            } else if(result.channels[i].position === 2){
+                var channelsNum = result.channels[i].name.replace(":", "");
+            }
+        }
+
+        $('.statsServersNum').text(serverNum);
+        $('.statsUsersNum').text(usersNum);
+        $('.statsChannelsNum').text(channelsNum);
+
+        console.log("[x] Data success. ")
+    });
+
+
+    $('.topBar-Radio').on('click', function() {
+        window.open("https://nentin.eu/radio","_self")
+    });
+
+    $('.topBar-GetBaron').on('click', function() {
+        window.open("https://discord.com/api/oauth2/authorize?client_id=883014007160717312&permissions=8&scope=bot%20applications.commands", '_blank');
+    });
+
+    $('.topBar-Donate').on('click', function() {
+        window.open("https://www.tipeeestream.com/nica-mihai/donation", '_blank');
+    });
+
+    $(".topBar2-Bug").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#Bug").offset().top
+        }, 2000);
+    });
+
+    $(".topBar2-Commands").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#commands").offset().top
+        }, 2000);
+    });
+
+    $(".topBar2-Features").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#features").offset().top
+        }, 2000);
+    });
+
+    $(".topBar2-About").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 2000);
+    });
+});
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show')
+      }
+    }
+  }
+}
